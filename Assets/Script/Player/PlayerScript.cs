af9878fa7673bb82e8cdc6f0769d4e55897e55bd
Evan <evan.rieber@gmail.com>
2025-06-28 12:12:22 +0200

    Base Movements

---
+++ Assets/Script/Player/PlayerScript.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    [Header("Movements")]
+    public int MoveSpeed;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    # region Updates
+    void Update()
+    {
+        MoveUpdate();
+    }
+
+    void MoveUpdate()
+    {
+        var horizontalMove = Input.GetAxis("Horizontal");
+        if (horizontalMove != 0)
+        {
+            transform.position += new Vector3(horizontalMove * MoveSpeed * Time.deltaTwime , 0, 0);
+        }
+    }
+    # endregion
+}
