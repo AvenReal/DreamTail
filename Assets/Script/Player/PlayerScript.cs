34c84158ee1a6f3d15fcb0cf7d400d50b4b81ccc
Evan <evan.rieber@gmail.com>
2025-07-07 10:51:34 +0200

    Fix FMOD bug + Added Dash + Added WalkingParticles

--- Assets/Script/Player/PlayerScript.cs
+++ Assets/Script/Player/PlayerScript.cs
@@ -2,6 +2,11 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+    [Header("Components")] 
+    public Rigidbody2D Rigidbody2D;
+    public ParticleSystem WalkingParticle;
+    
+    
     [Header("Movements")]
     public int MoveSpeed;
 
@@ -9,6 +14,17 @@ public class PlayerScript : MonoBehaviour
     public int JumpBoost;
     public int MaxNbOfJumps;
     public int NbOfJumps;
+
+    [Header("Dashes")] 
+    public int DashBoost;
+    public int MaxNbOfDashes;
+    public int NbOfDashes;
+    private bool DashDirection;
+    
+    [Header("WallJumps")]
+    public int WallJumpBoost;
+    public int MaxNbOfWallJumps;
+    public int NbOfWallJumps;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -20,15 +36,54 @@ public class PlayerScript : MonoBehaviour
     void Update()
     {
         MoveUpdate();
+        JumpUpdate();
+        DashUpdate();
     }
 
     void MoveUpdate()
     {
         var horizontalMove = Input.GetAxis("Horizontal");
+        DashDirection = horizontalMove > 0;
         if (horizontalMove != 0)
         {
             transform.position += new Vector3(horizontalMove * MoveSpeed * Time.deltaTime , 0, 0);
+            WalkingParticle.Play();
+        }
+        else
+        {
+            WalkingParticle.Stop();
         }
     }
+
+    void JumpUpdate()
+    {
+        if (!(Input.GetKeyDown(KeyCode.Space) && NbOfJumps > 0))
+            return;
+        
+        Rigidbody2D.AddForce(new Vector2(0, JumpBoost), ForceMode2D.Impulse);
+        NbOfJumps--;
+    }
+
+    void DashUpdate()
+    {
+        if(!(Input.GetKeyDown(KeyCode.E) && NbOfDashes > 0))
+            return;
+        
+        Rigidbody2D.AddForce(new Vector2((DashDirection ? 1 : -1) * DashBoost, 0), ForceMode2D.Impulse);
+        NbOfDashes--;
+
+    }
+    
     # endregion
+
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Floor"))
+        {
+            NbOfJumps = MaxNbOfJumps;
+            NbOfDashes = MaxNbOfDashes;
+            NbOfWallJumps = MaxNbOfWallJumps;
+        }
+    }
 }
